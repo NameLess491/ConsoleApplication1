f7e1259c7a8d2c9aafc2f5c72b606b797040d2e7
NameLess491 <dmitriy.mitnik99@gmail.com>
2024-09-06 18:40:31 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+struct Car {
+    std::string name;
+    std::string model;
+    double price;
+    std::string country;
+};
+
+int main() {
+    Car myCar;
+    myCar.name = "Toyota";
+    myCar.model = "Corolla";
+    myCar.price = 20000;
+    myCar.country = "Japan";
+
+    std::cout << "Car name: " << myCar.name << "\n";
+    std::cout << "Car model: " << myCar.model << "\n";
+    std::cout << "Car price: $" << myCar.price << "\n";
+    std::cout << "Made in: " << myCar.country << "\n";
+
+    return 0;
+}
